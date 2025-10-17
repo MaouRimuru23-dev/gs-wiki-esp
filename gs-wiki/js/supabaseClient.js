0d4977aa295526ccaea848e2690ed01748c47cb8
MaouRimuru23-dev <jiraiyasama2331@gmail.com>
2025-10-16 19:44:39 -0600

    Delete gs-wiki directory

--- gs-wiki/js/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-// public/js/supabaseClient.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-
-export const SUPABASE_URL = "https://TU-PROJECT-REF.supabase.co";
-export const SUPABASE_ANON_KEY = "TU-ANON-KEY";
-export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
